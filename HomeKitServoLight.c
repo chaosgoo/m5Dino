163b9027b35d08851dace04b1ebd9946e8941e9a
Goo <844268405@qq.com>
2020-05-12 11:03:07 +0800

    Update HomeKitServoLight.c

--- HomeKitServoLight.c
+++ HomeKitServoLight.c
@@ -1,3 +1,9 @@
+/* HomeKitServoLight
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
 #include <stdio.h>
 #include <esp_wifi.h>
 #include <esp_event_loop.h>
